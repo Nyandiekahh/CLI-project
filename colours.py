a8873bef4da8016ed5e466e2159ad7f24dacbd8a
cynthia <cyeeyearn@gmail.com>
2024-08-01 12:12:59 +0300

    ANSI escape codes

--- colours.py
+++ colours.py
@@ -1,7 +1,11 @@
+# ANSI color codes.
 class Colors:
-    """
-    ANSI color codes for console output.
-    """
-    GREEN = '\033[92m'
-    RED = '\033[91m'
-    RESET = '\033[0m'
+    HEADER = '\033[95m'   # Purple
+    OKBLUE = '\033[94m'   # Blue
+    OKCYAN = '\033[96m'   # Cyan
+    OKGREEN = '\033[92m'  # Green
+    WARNING = '\033[93m'  # Yellow
+    FAIL = '\033[91m'     # Red
+    BOLD = '\033[1m'      # Bold
+    UNDERLINE = '\033[4m' # Underline
+    RESET = '\033[0m'     # Reset to default
