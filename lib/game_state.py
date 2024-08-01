31fdad0463299c034391fce5374586f6a5d8e602
Tonny Mulli <88109276+TonnyMulli@users.noreply.github.com>
2024-08-01 11:42:13 +0300

    add player.py, game_state.py, colors.py (#6)

--- lib/game_state.py
+++ lib/game_state.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 class GameState:
     def __init__(self):
-        self.resources = {
-            "public_support": 50,
-            "equipment": 50,
-            "personnel": 50,
-            "morale": 50
-        }
-        self.current_location = "CBD"
-        self.score = 0
         self.player = None
-        self.high_score = 0
-
-    def update_location(self, new_location):
-        self.current_location = new_location
+        self.score = 0
+        self.current_location = "CBD"
+        self.resources = {
+            "personnel": 100,
+            "equipment": 100,
+            "public_support": 50,
+            "morale": 75
+        }
+        self.events = []
